66bf37910a19607db45d11e19acfc96638491378
liuvi01 <liuvi01@outlook.com>
2018-11-01 17:44:28 +0800

    initial one

---
+++ src/learnjava/helloworld.java
@@ -0,0 +1,14 @@
+package learnjava;
+
+public class helloworld {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.prinLn("hello world");
+	}
+
+}
